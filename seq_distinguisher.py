b021fee9e40d8088d2426144544e1d7148251291
AnastasiiaMurzina <hloshkin@mail.ru>
2019-01-09 21:06:36 +0300

    Update seq_distinguisher.py
    To run from console

--- seq_distinguisher.py
+++ seq_distinguisher.py
@@ -1,13 +1,14 @@
 from Bio import SeqIO
 import pandas as pd
+import sys
 
 comlement = {'A': 'T', 'C':'G', 'T':'A','G':'C', 'N':'N'}
-sequences = SeqIO.parse('GRCh38.primary_assembly.genome.fa', 'fasta')
-dfd_in = pd.read_csv('intronds_noverlapes.tsv', sep='\t', header=0)
-dfr_in = pd.read_csv('intronrs_noverlapes.tsv', sep='\t', header=0)
+sequences = SeqIO.parse(sys.argv[1], 'fasta')
 
-# with open('GRCh38_introns.fasta', 'w') as f:                                                                                                                                                               
-with open('G_introns.fasta', 'w') as f:                                                                                                                                                               
+dfd_in = pd.read_csv(sys.arv[2], sep='\t', header=0)
+dfr_in = pd.read_csv(sys.argv[3], sep='\t', header=0)
+
+with open(sys.argv[4], 'w') as f:                                                                                                                                                               
     for i in sequences:
         for j in dfd_in[dfd_in['chr'] == i.id].iterrows():
             f.write('>' +str(j[1]['transcript_id'])+'.'+str(int(j[1]['length']))+'.'+str(int(j[1]['exon_number']))+'\n')
@@ -18,7 +19,7 @@ with open('G_introns.fasta', 'w') as f:
             f.write('\n')
         for j in dfr_in[dfr_in['chr'] == i.id].iterrows():
             f.write('>' +str(j[1]['transcript_id']+'.'+str(j[1]['length'])) +'\n')
-            for k, ix in enumerate(str(i.seq[int(j[1]['start'])-1:int(j[1]['end'])][::-1]):
+            for k, ix in enumerate(str(i.seq[int(j[1]['start'])-1:int(j[1]['end']):-1])):
                     f.write(comlement[ix])
                     if k != 0 and k % 59 == 0:
                         f.write('\n')
